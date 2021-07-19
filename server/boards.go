7de220788ed6e6fde88eb1346c51900e8bb37563
Herover <hi+git@leonora.app>
2021-07-19 18:49:38 +0200

    Fix Board ID type

--- server/boards.go
+++ server/boards.go
@@ -1,10 +1,12 @@
 package main
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Board struct {
-	ID      string `json:"_id"`
-	Name    string `json:"name"`
-	Hidden  bool   `json:"hidden"`
-	Deleted bool   `json:"deleted"`
+	ID      primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Name    string             `json:"name"`
+	Hidden  bool               `json:"hidden"`
+	Deleted bool               `json:"deleted"`
 }
 
 type NewBoard struct {
