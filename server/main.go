e346ed7a9e01a8b91e8c9307a506e149aa2d23cd
Herover <hi+git@leonora.app>
2021-07-19 18:50:18 +0200

    Add more information to logs
    IP and HTTP method

--- server/main.go
+++ server/main.go
@@ -67,7 +67,7 @@ func main() {
 	var corsMiddleware = func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
-			log.Println(r.RequestURI)
+			log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.RequestURI)
 			next.ServeHTTP(w, r)
 		})
 	}
